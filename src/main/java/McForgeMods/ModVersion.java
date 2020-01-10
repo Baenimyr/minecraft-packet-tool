f1b00efc4f2fc06c9bd86fed3ebfe3e372629fd3
Benjamin Le Rohellec <benjamin@lerohellec.nom.fr>
2020-01-10 20:42:05 +0100

    ModVersion.java: l'url est maintenant une liste d'urls possibles

--- src/main/java/McForgeMods/ModVersion.java
+++ src/main/java/McForgeMods/ModVersion.java
@@ -1,14 +1,19 @@
 package McForgeMods;
 
 import McForgeMods.depot.Depot;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-/** Cette classe incarne une version particulière d'un mod.
+/**
+ * Cette classe incarne une version particulière d'un mod.
  * Elle rassemble toutes les informations relatives à une version, n'apparaissant pas dans {@link Mod}.
- * L'association à un fichier réel {@link #url} est optionnelle.
+ * L'association à un fichier réel {@link #urls} est optionnelle.
  *
  * @see Mod
  */
@@ -17,7 +22,8 @@ public class ModVersion {
     public final Version version;
     public final Version mcversion;
 
-    String url = null;
+    /** Liste des liens menant au fichier (localement ou un url) */
+    public List<URL> urls = new ArrayList<>(1);
     String parent = null;
     /**
      * Mods obligatoires pour le bon fonctionnement de celui-ci.
@@ -48,7 +54,23 @@ public class ModVersion {
         this.version = Version.read(json.getString("version"));
         this.mcversion = Version.read(json.getString("mcversion"));
 
-        url = json.has("url") ? json.getString("url") : null;
+        if (json.has("urls")) {
+            Object url = json.get("urls");
+            if (url instanceof JSONArray) {
+                for (int i = 0; i < ((JSONArray) url).length(); i++)
+                    try {
+                        this.urls.add(new URL(((JSONArray) url).getString(i)));
+                    } catch (MalformedURLException u) {
+                        u.printStackTrace();
+                    }
+            } else {
+                try {
+                    this.urls.add(new URL(json.getString("urls")));
+                } catch (MalformedURLException u) {
+                    u.printStackTrace();
+                }
+            }
+        }
     }
 
     public void json(JSONObject json) {
@@ -56,8 +78,8 @@ public class ModVersion {
         json.put("version", this.version);
         json.put("mcversion", this.mcversion);
 
-        if (this.url != null)
-            json.put("url", this.url);
+        if (this.urls != null)
+            json.put("urls", new JSONArray(this.urls));
     }
 
     @Override
