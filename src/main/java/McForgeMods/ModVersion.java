ff1de1e52bff0ced677bded9545a5071976f8477
Benjamin Le Rohellec <benjamin@lerohellec.nom.fr>
2020-01-14 22:06:31 +0100

    ModVersion: uniformisation des enregistrements.
    Non ajout des doublons.
    Tri alphabétique des listes enregistrées dans le JSON.

--- src/main/java/McForgeMods/ModVersion.java
+++ src/main/java/McForgeMods/ModVersion.java
@@ -15,25 +15,24 @@ import java.util.*;
  * @see Mod
  */
 public class ModVersion {
-    public final Mod mod;
-    public final Version version;
-    public final Version mcversion;
-
-    /**
-     * Liste des liens menant au fichier (localement ou un url)
-     */
-    public final List<URL> urls = new ArrayList<>(1);
-    String parent = null;
-    /**
-     * Mods obligatoires pour le bon fonctionnement de celui-ci.
-     * Une intervalle de version doit être spécifiée.
-     */
-    public final Map<String, VersionIntervalle> requiredMods = new HashMap<>();
-    /**
-     * Mods, si présents à charger avant.
-     * Aucune intervalle de version n'est nécessaire.
-     */
-    public final List<String> dependants = new ArrayList<>();
+	public final Mod     mod;
+	public final Version version;
+	public final Version mcversion;
+	
+	/**
+	 * Liste des liens menant au fichier (localement ou un url)
+	 */
+	public final List<URL>                      urls         = new ArrayList<>(1);
+	/**
+	 * Mods obligatoires pour le bon fonctionnement de celui-ci.
+	 * Une intervalle de version doit être spécifiée.
+	 */
+	public final Map<String, VersionIntervalle> requiredMods = new HashMap<>();
+	/**
+	 * Mods, si présents, à charger avant celui-ci.
+	 * Aucune intervalle de version n'est nécessaire.
+	 */
+	public final List<String>                   dependants   = new ArrayList<>();
 
     /*
     List<String> authorList;
@@ -41,102 +40,137 @@ public class ModVersion {
     String logoFile;
     List<String> screenshots;
      */
-
-    public ModVersion(Mod mod, Version version, Version mcversion) {
-        this.mod = mod;
-        this.version = version;
-        this.mcversion = mcversion;
-    }
-
-    /**
-     * Constructeur utilisé pour initialisé une version à partir des informations d'un dépot.
-     */
-    public ModVersion(Mod mod, Version version, JSONObject json) {
-        this.mod = mod;
-        this.version = version;
-        this.mcversion = Version.read(json.getString("mcversion"));
-
-        if (json.has("urls")) {
-            Object url = json.get("urls");
-            if (url instanceof JSONArray) {
-                for (int i = 0; i < ((JSONArray) url).length(); i++)
-                    try {
-                        this.urls.add(new URL(((JSONArray) url).getString(i)));
-                    } catch (MalformedURLException u) {
-                        u.printStackTrace();
-                    }
-            } else {
-                try {
-                    this.urls.add(new URL(json.getString("urls")));
-                } catch (MalformedURLException u) {
-                    u.printStackTrace();
-                }
-            }
-        }
-
-        if (json.has("requiredMods")) {
-            JSONArray liste = json.getJSONArray("requiredMods");
-            for (int i = 0; i < liste.length(); i++) {
-                String required = liste.getString(i);
-                int at = required.indexOf('@');
-                if (at != -1) {
-                    this.requiredMods.put(required.substring(0, at).toLowerCase(),
-                            VersionIntervalle.read(required.substring(at + 1)));
-                } else {
-                    this.requiredMods.put(required, new VersionIntervalle());
-                }
-            }
-        }
-
-        if (json.has("dependants")) {
-            JSONArray liste = json.getJSONArray("dependants");
-            for (int i = 0; i < liste.length(); i++) {
-                this.dependants.add(liste.getString(i));
-            }
-        }
-    }
-
-    public void json(JSONObject json) {
-        json.put("mcversion", this.mcversion);
-
-        json.put("urls", new JSONArray(this.urls));
-
-        JSONArray liste = new JSONArray();
-        this.requiredMods.forEach((id, inter) -> liste.put(id + "@" + inter));
-        json.put("requiredMods", liste);
-
-        if (!this.dependants.isEmpty()) {
-            JSONArray dep = new JSONArray(this.dependants);
-            json.put("dependants", dep);
-        }
-    }
-
-    public void fusion(ModVersion autre) {
-        for (URL url : autre.urls)
-            if (!this.urls.contains(url))
-                this.urls.add(url);
-
-        this.requiredMods.putAll(autre.requiredMods);
-        for (String dep : autre.dependants)
-            if (!this.dependants.contains(dep))
-                this.dependants.add(dep);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ModVersion that = (ModVersion) o;
-        return mod.equals(that.mod) && version.equals(that.version);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(mod, version);
-    }
-
-    @Override
-    public String toString() {
-        return "ModVersion{" + "mod=" + mod.name + ", version=" + version + ", mcversion=" + mcversion + '}';
-    }
+	
+	public ModVersion(Mod mod, Version version, Version mcversion) {
+		this.mod = mod;
+		this.version = version;
+		this.mcversion = mcversion;
+	}
+	
+	/**
+	 * Constructeur utilisé pour initialisé une version à partir des informations d'un dépot.
+	 */
+	public ModVersion(Mod mod, Version version, JSONObject json) {
+		this.mod = mod;
+		this.version = version;
+		this.mcversion = Version.read(json.getString("mcversion"));
+		
+		if (json.has("urls")) {
+			Object url = json.get("urls");
+			if (url instanceof JSONArray) {
+				for (int i = 0; i < ((JSONArray) url).length(); i++)
+					try {
+						this.ajoutURL(new URL(((JSONArray) url).getString(i)));
+					} catch (MalformedURLException u) {
+						u.printStackTrace();
+					}
+			} else {
+				try {
+					this.ajoutURL(new URL(json.getString("urls")));
+				} catch (MalformedURLException u) {
+					u.printStackTrace();
+				}
+			}
+		}
+		
+		if (json.has("requiredMods")) {
+			JSONArray liste = json.getJSONArray("requiredMods");
+			for (int i = 0; i < liste.length(); i++) {
+				String required = liste.getString(i);
+				int at = required.indexOf('@');
+				if (at != -1) {
+					this.ajoutModRequis(required.substring(0, at), VersionIntervalle.read(required.substring(at + 1)));
+				} else {
+					this.ajoutModRequis(required, new VersionIntervalle());
+				}
+			}
+		}
+		
+		if (json.has("dependants")) {
+			JSONArray liste = json.getJSONArray("dependants");
+			for (int i = 0; i < liste.length(); i++) {
+				this.ajoutDependant(liste.getString(i));
+			}
+		}
+	}
+	
+	/**
+	 * Ajoute un url sans doublons.
+	 */
+	public void ajoutURL(URL url) {
+		if (!this.urls.contains(url)) this.urls.add(url);
+	}
+	
+	/**
+	 * Ajoute une nouvelle dépendance.
+	 * Si un mod identique est déjà enregistrée, enregistre l'intersection entre les deux intervalles de version.
+	 *
+	 * @param modid      requis
+	 * @param intervalle pour laquelle le modid est requis.
+	 */
+	public void ajoutModRequis(String modid, VersionIntervalle intervalle) {
+		modid = modid.toLowerCase().intern();
+		if (this.requiredMods.containsKey(modid)) this.requiredMods.get(modid).fusion(intervalle);
+		else this.requiredMods.put(modid, intervalle);
+	}
+	
+	/**
+	 * Ajoute un nouveau mod comme dépendant du mod présent.
+	 * Ajout sans doublons.
+	 */
+	public void ajoutDependant(String modid) {
+		modid = modid.toLowerCase().intern();
+		if (!this.dependants.contains(modid)) this.dependants.add(modid);
+	}
+	
+	/**
+	 * Écrit le contenu du json associé à cette version.
+	 *
+	 * @param json: zone d'écriture
+	 */
+	public void json(JSONObject json) {
+		this.urls.sort(Comparator.comparing(URL::toString));
+		this.dependants.sort(String::compareTo);
+		
+		json.put("mcversion", this.mcversion);
+		json.put("urls", new JSONArray(this.urls));
+		
+		JSONArray liste = new JSONArray();
+		this.requiredMods.keySet().stream().sorted().forEach(id -> liste.put(id + "@" + this.requiredMods.get(id)));
+		json.put("requiredMods", liste);
+		
+		if (!this.dependants.isEmpty()) {
+			JSONArray dep = new JSONArray(this.dependants);
+			json.put("dependants", dep);
+		}
+	}
+	
+	/**
+	 * Importe les nouvelles données à partir d'une instance similaire.
+	 */
+	public void fusion(ModVersion autre) {
+		this.requiredMods.putAll(autre.requiredMods);
+		for (URL url : autre.urls)
+			this.ajoutURL(url);
+		for (String dep : autre.dependants)
+			this.ajoutDependant(dep);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		ModVersion that = (ModVersion) o;
+		return mod.equals(that.mod) && version.equals(that.version);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(mod, version);
+	}
+	
+	@Override
+	public String toString() {
+		return "ModVersion{" + "mod=" + mod.name + ", version=" + version + ", mcversion=" + mcversion + '}';
+	}
 }
