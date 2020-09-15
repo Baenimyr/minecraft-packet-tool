cf88779f3391ff62dc340e30f563269afb5dcc02
Benjamin Le Rohellec <benjamin@lerohellec.nom.fr>
2020-09-15 20:16:35 +0200

    remove: sécurité des dépendances

--- src/main/java/McForgeMods/commandes/CommandeRemove.java
+++ src/main/java/McForgeMods/commandes/CommandeRemove.java
@@ -8,15 +8,19 @@ import picocli.CommandLine;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "remove")
+@CommandLine.Command(name = "remove", resourceBundle = "mcforgemods/lang/Remove")
 public class CommandeRemove implements Callable<Integer> {
 	@CommandLine.Mixin
 	ForgeMods.DossiersOptions dossiers;
 	
 	@CommandLine.Parameters(arity = "1..n", descriptionKey = "mods")
 	List<String> mods;
+	
+	@CommandLine.Option(names = {"-f", "--force"})
+	boolean force;
 	
 	@Override
 	public Integer call() {
@@ -25,14 +29,26 @@ public class CommandeRemove implements Callable<Integer> {
 		
 		depotInstallation.statusImportation();
 		
+		boolean erreur = false;
 		for (String id : mods) {
 			if (!depotInstallation.contains(id)) {
-				System.err.printf("%s n'est pas installé !", id);
-				continue;
+				System.err.printf("%s n'est pas installé !%n", id);
+				erreur = true;
 			}
+			if (!force && depotInstallation.getModids().stream().filter(i -> !mods.contains(i))
+					.map(depotInstallation::informations).map(i -> depotLocal.getModVersion(i.modid, i.version))
+					.filter(Optional::isPresent).map(Optional::get).anyMatch(mv -> mv.requiredMods.containsKey(id))) {
+				System.err.printf("Impossible de supprimer '%s' car il est une dépendance.%n", id);
+				erreur = true;
+			}
+		}
+		if (erreur) return 1;
+		
+		for (String id : mods) {
 			try {
+				System.out.println("Suppression de " + id);
 				if (!depotInstallation.desinstallation(id)) {
-					System.err.printf("Impossible de supprimer %s: aucune données", id);
+					System.err.printf("Impossible de supprimer %s: aucune données%n", id);
 				}
 			} catch (FileSystemException e) {
 				e.printStackTrace();
@@ -44,7 +60,7 @@ public class CommandeRemove implements Callable<Integer> {
 			depotInstallation.close();
 		} catch (IOException e) {
 			System.err.println("Impossible de sauvegarder la configuration de l'installation.");
-			return 1;
+			return 2;
 		}
 		return 0;
 	}
